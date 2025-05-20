c40ab13ddb15fb999d5d668087df2bdf4bbc5c51
93mmm <nickolaev513@gmail.com>
2025-05-20 17:01:45 +0300

    config validation improved

--- internal/config/validate.go
+++ internal/config/validate.go
@@ -3,31 +3,38 @@ package config
 import (
 	"errors"
 	"fmt"
+	"log"
 )
 
 func genError(name string) error {
-	return fmt.Errorf("Env %v is not valid", name)
+	return fmt.Errorf("Env %v is not valid or missing", name)
+}
+
+func isDefault(in any) bool {
+	switch val := in.(type) {
+	case string:
+		return len(val) == 0
+	case int:
+		return val == 0
+	default:
+		log.Println("[WARN] Unknown type of field in config")
+		return false
+	}
 }
 
 func (c *config) validate() error {
 	fmt.Println(c)
 	var errs []error
 
-	if len(c.App.Host) == 0 {
-		errs = append(errs, genError("APP_HOST"))
+	check := func(in any, env string) {
+		if isDefault(in) {
+			errs = append(errs, genError(env))
+		}
 	}
-
-	if c.App.Port == 0 {
-		errs = append(errs, genError("APP_PORT"))
-	}
-
-	if len(c.Mongo.Host) == 0 {
-		errs = append(errs, genError("MONGO_HOST"))
-	}
-
-	if c.Mongo.Port == 0 {
-		errs = append(errs, genError("MONGO_PORT"))
-	}
+	check(c.App.Host, "APP_HOST")
+	check(c.App.Port, "APP_PORT")
+	check(c.Mongo.Host, "MONGO_HOST")
+	check(c.Mongo.Port, "MONGO_PORT")
 
 	if errs != nil {
 		return errors.Join(errs...)
